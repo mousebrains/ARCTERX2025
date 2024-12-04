a6446380b6f7233a7aa015019182c01da869b9d5
Pat Welch <pat@mousebrains.com>
2024-12-04 02:05:00 +0000

    partition error handling

--- WAMV/listen.py
+++ WAMV/listen.py
@@ -39,7 +39,7 @@ parser.add_argument("--tgtPort", type=int, default="31337",
                     help="Where to forward datagrams to");
 args = parser.parse_args()
 
-Logger.mkLogger(args)
+Logger.mkLogger(args, fmt="%(asctime)s %(levelname)s: %(message)s")
 
 args.csv = os.path.abspath(os.path.expanduser(args.csv))
 csvDir = os.path.dirname(args.csv)
@@ -62,12 +62,14 @@ dbArg = f"dbname={args.db} user={args.username}"
 sql = "INSERT INTO position (name, time, latitude, longitude) VALUES (%s,%s,%s,%s)"
 sql+= " ON CONFLICT DO NOTHING;"
 
+logging.info("Starting");
+
 with psycopg.connect(dbArg) as conn, conn.cursor() as cur:
     mt.mkPosition(cur)
 
 while True:
     (data, addr) = src.recvfrom(1024) # Buffer size
-    print(data, addr)
+    logging.info("data %s addr %s", data, addr)
     if not data: continue
     fields = data.split(b",")
     if len(fields) != 5:
@@ -77,29 +79,44 @@ while True:
     try:
         time = str(time, "utf-8")
     except:
+        logging.warning("Unable to convert %s to a string", time)
         continue
-    payload = json.dumps(dict(
-        name = "WAM-V",
-        time = time,
-        lat = mkDMS(lat, ("N", "S")),
-        lon = mkDMS(lon, ("E", "W")),
-        sog = float(sog),
-        cog = float(cog),
-        ))
-    logging.info("Payload %s", payload)
-    tgt.sendto(bytes(payload + "\n", "utf-8"), tgtAddr)
 
-    time = datetime.fromisoformat(time).replace(tzinfo=timezone.utc)
-    lat = float(lat)
-    lon = float(lon)
-    with psycopg.connect(dbArg) as conn, conn.cursor() as cur:
-        cur.execute("BEGIN TRANSACTION;")
-        cur.execute(sql, ("wamv", time, lat, lon))
-        conn.commit()
+    try:
+        payload = json.dumps(dict(
+            name = "WAM-V",
+            time = time,
+            lat = mkDMS(lat, ("N", "S")),
+            lon = mkDMS(lon, ("E", "W")),
+            sog = float(sog),
+            cog = float(cog),
+            ))
+        logging.info("Payload %s", payload)
+        tgt.sendto(bytes(payload + "\n", "utf-8"), tgtAddr)
+    except:
+        logging.exception("Failed in payload and send")
 
-    with open(args.csv, "a") as fp:
-        t = time.timestamp()
-        fp.write(f"{t:.0f},{lat:.6f},{lon:.6f}\n")
+    try:
+        time = datetime.fromisoformat(time).replace(tzinfo=timezone.utc)
+        lat = float(lat)
+        lon = float(lon)
+    except:
+        logging.exception("Failed in conversion")
+
+    try:
+        with psycopg.connect(dbArg) as conn, conn.cursor() as cur:
+            cur.execute("BEGIN TRANSACTION;")
+            cur.execute(sql, ("wamv", time, lat, lon))
+            conn.commit()
+    except:
+        logging.exception("Failed in DB update")
+
+    try:
+        with open(args.csv, "a") as fp:
+            t = time.timestamp()
+            fp.write(f"{t:.0f},{lat:.6f},{lon:.6f}\n")
+    except:
+        logging.exception("Failed in writing CSV")
 
 
 
