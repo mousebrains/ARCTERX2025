ebc0d80c15c98c1d8b67080a6cd544099e931946
Pat Welch <pat@mousebrains.com>
2025-01-15 05:10:15 +0000

    Cleanup

--- MakeTables/MakeTables.py
+++ MakeTables/MakeTables.py
@@ -22,26 +22,62 @@ def qTableExists(cur:psycopg.Cursor, tbl:str, schema="public") -> bool:
     result = cur.fetchone()
     return result[0] if result else False
 
-def mkPosition(cur:psycopg.Cursor, tbl:str = "position") -> str:
-    sql =f"CREATE TABLE IF NOT EXISTS {tbl}("
-    sql+= "  time TIMESTAMP WITH TIME ZONE NOT NULL,"
-    sql+= "  name TEXT NOT NULL,"
-    sql+= "  latitude NUMERIC, CHECK(latitude >= -90 AND latitude <= 90),"
-    sql+= "  longitude NUMERIC, CHECK(longitude >= -180 AND longitude <= 180),"
-    sql+= "  qCSV boolean DEFAULT False,"
-    sql+= "  qNC boolean DEFAULT False,"
-    sql+= "  PRIMARY KEY(time, name)"
-    sql+= ");"
+def mkPosition(cur:psycopg.Cursor) -> str:
+    tbl = "position" # Two places, here and in the sql
+    sql = """
+-------- 
+
+-- single table for all the positions
+
+CREATE TABLE IF NOT EXISTS position (
+  time TIMESTAMP WITH TIME ZONE NOT NULL,
+  name TEXT NOT NULL,
+  class TEXT NOT NULL,
+  latitude NUMERIC, CHECK(latitude >= -90 AND latitude <= 90),
+  longitude NUMERIC, CHECK(longitude >= -180 AND longitude <= 180),
+  qCSV boolean DEFAULT False,
+  qNC boolean DEFAULT False,
+  PRIMARY KEY(time, name, class)
+);
+
+CREATE INDEX IF NOT EXISTS  pos_class_name ON position (class, name);
+CREATE INDEX IF NOT EXISTS  pos_name ON position (name);
+CREATE INDEX IF NOT EXISTS  pos_qCSV ON position (qCSV);
+CREATE INDEX IF NOT EXISTS  pos_qNC ON position (qNC);
+
+-- Function sends a notification whenever position is updated
+
+CREATE OR REPLACE FUNCTION pos_updated_func() 
+  RETURNS  TRIGGER AS $psql$
+BEGIN
+  PERFORM pg_notify('pos_updated', 'pos');
+  RETURN NEW;
+end;
+$psql$ 
+LANGUAGE plpgsql;
+
+-- When position is updated or inserted into, call the nofiication function
+
+CREATE OR REPLACE TRIGGER pos_updated AFTER INSERT OR UPDATE 
+  ON position
+  FOR EACH STATEMENT
+    EXECUTE PROCEDURE pos_updated_func();
+
+--------
+    """
 
     cur.execute(sql)
 
     return tbl if cur.statusmessage == "CREATE TABLE" else None
 
-def mkFilePosition(cur:psycopg.Cursor, tbl:str="fileposition") -> str:
-    sql =f"CREATE TABLE IF NOT EXISTS {tbl}("
-    sql+= "  filename TEXT PRIMARY KEY,"
-    sql+= "  position BIGINT NOT NULL"
-    sql+= ");"
+def mkFilePosition(cur:psycopg.Cursor) -> str:
+    tbl = "fileposition" # Two places, here and the SQL
+    sql = """
+CREATE TABLE IF NOT EXISTS fileposition (
+	filename TEXT PRIMARY KEY,
+	position BIGINT NOT NULL
+);
+    """
 
     cur.execute(sql)
 
@@ -63,4 +99,5 @@ if __name__ == "__main__":
     parser.add_argument("--user", type=str, default="pat", help="Database user to work with")
     args = parser.parse_args()
 
+    logging.basicConfig(level=logging.DEBUG)
     mkAll(args.db, args.user)
