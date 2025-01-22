358408a1cc2209b5cdb9f29e384b7df91eaaadf7
Pat Welch <pat@mousebrains.com>
2025-01-22 04:15:53 +0000

    Change to pandas

--- Thompson/scs2NC.py
+++ Thompson/scs2NC.py
@@ -15,8 +15,6 @@ import re
 import time
 import numpy as np
 import pandas as pd
-from mkNC import createNetCDF
-from netCDF4 import Dataset
 import psycopg
 import sys
 from ncWriter import ncWriter
@@ -24,10 +22,10 @@ import yaml
 
 def mkFilenames(paths:tuple, cur) -> dict:
     patterns = {
-        "MET": re.compile(r"^(SONIC-TWIND|PAR|BOW-MET|RAD)-RAW_([0-9]+)-[0-9]+"),
-        "NAV": re.compile(r"^CNAV3050-(GGA|VTG)-RAW_([0-9]+)-[0-9]+"),
-        "SEAWATER": re.compile(r"^(FLUOROMETER|TSG|SBE38)-RAW_([0-9]+)-[0-9]+"),
-        "SOUNDERS": re.compile(r"^(KNUDSEN-PKEL99-RAW|MB-DEPTH)_([0-9]+)-[0-9]+"),
+            "MET": re.compile(r"^(SONIC-TWIND-RAW|PAR-RAW|BOW-MET-RAW|RAD|Campbell-RAD|BRIDGE-WIND-(STBD|PORT)-DRV-Data)_(\d+)-\d+.Raw$"),
+            "NAV": re.compile(r"^CNAV3050-(GGA|VTG)-RAW_(\d+)-\d+.Raw$"),
+            "SEAWATER": re.compile(r"^(FLUOROMETER|TSG|SBE38)-RAW_(\d+)-\d+.Raw$"),
+            "SOUNDERS": re.compile(r"^(KNUDSEN-PKEL99-RAW|MB-DEPTH)_(\d+)-\d+.Raw$"),
         }
 
     sql = "SELECT position FROM fileposition WHERE filename=%s;"
@@ -45,8 +43,8 @@ def mkFilenames(paths:tuple, cur) -> dict:
                     pos = row[0]
                     break
 
-                if pos and os.path.getsize(fn) == pos: continue
-                date = matches[2]
+                if pos is not None and os.path.getsize(fn) == pos: continue
+                date = matches[matches.lastindex]
                 if date not in items: items[date] = {}
                 items[date][fn] = (pos, matches[1])
     return items
@@ -70,44 +68,52 @@ def decodeFloat(val:str, norm:float=1.0) -> float:
     except:
         return None
 
-def procTWind(fields:tuple) -> dict:
-    return {"wSpd": decodeFloat(fields[3]), "wDir": decodeFloat(fields[4])}
+def procTWind(fields:tuple, codigo:str) -> dict:
+    if "-STBD-" in codigo:
+        suffix = "Stbd"
+    elif "-PORT-" in codigo:
+        suffix = "Port"
+    else:
+        suffix = ""
 
-def procGGA(fields:tuple) -> dict:
+    return {"wSpd" + suffix: decodeFloat(fields[3]), 
+            "wDir" + suffix: decodeFloat(fields[4])}
+
+def procGGA(fields:tuple, codigo:str) -> dict:
     return {
             "lat": decodeDegMin(fields[4], fields[5]),
             "lon": decodeDegMin(fields[6], fields[7]),
             }
 
-def procVTG(fields:tuple) -> dict:
+def procVTG(fields:tuple, codigo:str) -> dict:
     return {
             "cog": decodeFloat(fields[3]),
             "sog": decodeFloat(fields[7], 1852/3600), # knots -> meters/sec
             }
 
-def procPAR(fields:tuple) -> dict:
+def procPAR(fields:tuple, codigo:str) -> dict:
     return {
             "par": decodeFloat(fields[3]),
             }
 
-def procSpeedOfSound(fields:tuple) -> dict:
+def procSpeedOfSound(fields:tuple, codigo:str) -> dict:
     return {
             "spdSound": decodeFloat(fields[2]),
             }
 
-def procSBE38(fields:tuple) -> dict:
+def procSBE38(fields:tuple, codigo:str) -> dict:
     return {
-            "Tinlet": decodeFloat(fields[2]),
+            "temperatureInlet": decodeFloat(fields[2]),
             }
 
-def procTSG(fields:tuple) -> dict:
+def procTSG(fields:tuple, codigo:str) -> dict:
     return {
-            "Ttsg": decodeFloat(fields[2]),
-            "cond": decodeFloat(fields[3]),
+            "temperatureTSG": decodeFloat(fields[2]),
+            "conductivity": decodeFloat(fields[3]),
             "salinity": decodeFloat(fields[4]),
             }
 
-def procFluorometer(fields:tuple) -> dict:
+def procFluorometer(fields:tuple, codigo:str) -> dict:
     items = fields[2].split("\t")
     if len(items) < 6: return None
     return {
@@ -115,39 +121,46 @@ def procFluorometer(fields:tuple) -> dict:
             "flThermistor": int(items[5]),
             }
 
-def procBowMet(fields:tuple) -> dict:
+def procBowMet(fields:tuple, codigo:str) -> dict:
     return {
-            "Tair": decodeFloat(fields[5]),
+            "temperatureAir": decodeFloat(fields[5]),
             "RH": decodeFloat(fields[6]),
-            "Pair": decodeFloat(fields[7]),
+            "pressureAir": decodeFloat(fields[7]),
             }
 
-def procRadiation(fields:tuple) -> dict:
+def procCampbellRadiation(fields:tuple, codigo:str) -> dict:
+    return {
+            "longWave": decodeFloat(fields[4]),
+            "shortWave": decodeFloat(fields[6]),
+            }
+
+def procRadiation(fields:tuple, codigo:str) -> dict:
     return {
             "longWave": decodeFloat(fields[7]),
             "shortWave": decodeFloat(fields[10]),
             }
 
-def procPKEL99(fields:tuple) -> dict:
+def procPKEL99(fields:tuple, codigo:str) -> dict:
     if fields[4] != '0': return None
     return {
             "depthKN": decodeFloat(fields[3]),
             }
 
-def procMBdepth(fields:tuple) -> dict:
+def procMBdepth(fields:tuple, codigo:str) -> dict:
     return {
             "depthMB": decodeFloat(fields[3]),
             }
 
 codigos = {
-        "$TWIND": procTWind,
+        "$DEPTH": procMBdepth,
         "$GPGGA": procGGA,
         "$GPVTG": procVTG,
-        "$PPAR": procPAR,
         "$METED": procBowMet,
-        "$WIR37": procRadiation,
         "$PKEL99": procPKEL99,
-        "$DEPTH": procMBdepth,
+        "$PPAR": procPAR,
+        "$RAD": procCampbellRadiation,
+        "$TWIND": procTWind,
+        "$WIR37": procRadiation,
         "SBE38": procSBE38,
         "TSG": procTSG,
         "SS": procSpeedOfSound,
@@ -155,142 +168,114 @@ codigos = {
         }
 
 def procLine(line:str, codigo:str=None) -> dict:
+    codigoOrig = codigo
+
     if not line: return None
     fields = line.strip().split(",")
     if len(fields) < 3: return None
     try:
-        t = datetime.datetime.strptime(fields[0] + " " + fields[1], "%m/%d/%Y %H:%M:%S.%f")
-        tt = t.replace(microsecond=0)
-        if t.microsecond >= 500000: tt += datetime.timedelta(seconds=1)
-        t  = np.datetime64(t)
-        tt = np.datetime64(tt)
-        dt = (t - tt).astype("timedelta64[ms]").astype(float) / 1000
+        t = datetime.datetime.strptime(fields[0] + " " + fields[1], "%m/%d/%Y %H:%M:%S.%f") \
+                .replace(tzinfo=datetime.timezone.utc)
+        dSeconds = round(t.microsecond/1000000)
+        tt = t.replace(microsecond=0) + datetime.timedelta(seconds=dSeconds)
 
         if fields[2][0] == "$": codigo = fields[2]
 
         if codigo not in codigos:
-            logging.warning("Unsupported record type, %s", codigo)
+            logging.warning("Unsupported record type, %s, %s", codigo, line.strip())
             return None
 
-        val = codigos[codigo](fields)
-        return ({"t": tt, "dt": dt} | val) if val else None
+        val = codigos[codigo](fields, codigoOrig)
+        return ({"t": tt} | val) if val else None
     except:
         logging.exception("codigo %s Fields %s", codigo, fields)
 
-def loadFile(fn:str, pos:int, codigo:str) -> tuple:
-    items = []
-    with open(fn, "r") as fp:
-        if pos: fp.seek(pos)
-        for line in fp:
-            val = procLine(line, codigo)
-            if val: items.append(val)
-        pos = fp.tell()
-    if not items: return (None, pos) # In case there is nothing
-    df = pd.DataFrame(items)
-    return (df, pos)
+def loadFile(fn:str, pos:int, codigo:str, nc:ncWriter) -> tuple:
+    try:
+        items = []
+        with open(fn, "r") as fp:
+            if pos: fp.seek(pos)
+            for line in fp:
+                val = procLine(line, codigo)
+                if val: items.append(val)
+            pos = fp.tell()
+        if not items: return (None, pos) # In case there is nothing
+        df = pd.DataFrame(items)
+        return (df, pos)
+    except:
+        logging.exception("Working on %s", fn)
+        return (None, pos)
 
-def saveDataframe(nc, df:pd.DataFrame, tBase:np.int64) -> None:
-    t = (df.t - tBase).astype("timedelta64[s]").astype(np.int64)
-
-    nc.variables["t"][t] = t
-
-    for key in df:
-        if key in ["t", "dt"]: continue
-        nc.variables[key][t] = df[key].to_numpy()
-   
-def getTimeOffset(nc) -> np.int64:
-    units = nc.variables["t"].getncattr("units")
-    since = "since "
-    index = units.find(since) + len(since)
-    return np.datetime64(units[index:])
-
-def loadIt(paths:list, args:ArgumentParser) -> None:
-    ncPath = args.nc
+def loadIt(paths:list, args:ArgumentParser, nc:ncWriter) -> None:
     dbName = args.db
-    copyTo = args.copyTo
-    bufferSize = args.bufferSize
 
     sql = "INSERT INTO filePosition VALUES (%s, %s)"
     sql+= " ON CONFLICT (filename) DO UPDATE SET position=EXCLUDED.position;"
 
-    sfn = os.path.join(ncPath, "ship.nc") # Global set
-    toCopy = set()
-
     with psycopg.connect(f"dbname={dbName}") as db:
         cur = db.cursor()
         filenames = mkFilenames(paths, cur)
+        cur.execute("BEGIN TRANSACTION;")
 
         for date in sorted(filenames):
-            ofn = os.path.join(ncPath, f"ship.{date}.nc")
-            logging.info("Working on %s %s -> %s", date, len(filenames[date]), ofn)
-            frames = []
-            cur.execute("BEGIN TRANSACTION;")
+            logging.info("Working on %s %s", date, len(filenames[date]))
+            frames = None
             for fn in filenames[date]:
                 t0 = time.time()
-                (df, pos) = loadFile(fn, filenames[date][fn][0], filenames[date][fn][1])
+                (df, pos) = loadFile(fn, filenames[date][fn][0], filenames[date][fn][1], nc)
                 if df is None:
                     logging.info("No data from %s", fn)
+                    if pos is not None:
+                        cur.execute(sql, (fn, pos))
                     continue
+                if not df.empty:
+                    if frames is None:
+                        frames = df
+                    else:
+                        frames = frames.merge(df, how="outer", on="t")
                 t1 = time.time()
                 logging.info("Loaded %s in %s secs, sz %s pos %s", 
-                             os.path.basename(fn), round(t1-t0,1), df.t.size, pos)
+                             os.path.basename(fn), round(t1-t0,1), len(df), pos)
                 cur.execute(sql, (fn, pos))
-                if df is not None and not df.empty: 
-                    frames.append(df)
-   
-            for fn in [sfn, ofn]:
-                if not os.path.isfile(fn): 
-                    tMin = None
-                    for df in frames:
-                        tMin = df.t.min() if tMin is None else min(tMin, df.t.min())
-                    if not tMin: continue
-                    createNetCDF(fn, tMin.to_datetime64())
-
-                with Dataset(fn, "a") as nc:
-                    tBase = getTimeOffset(nc)
-                    for df in frames: saveDataframe(nc, df, tBase)
-
-                toCopy.add(fn)
-
-            db.commit()
-
-        if copyTo:
-            for fn in toCopy:
-                cfn = os.path.join(copyTo, os.path.basename(fn))
-                totSize = 0
-                stime = time.time()
-                with NamedTemporaryFile(dir=copyTo, delete=False) as ofp, open(fn, "rb") as ifp:
-                    tfn = ofp.name
-                    buffer = ifp.read(bufferSize)
-                    while buffer:
-                        totSize += ofp.write(buffer)
-                        buffer = ifp.read(bufferSize)
-                os.replace(tfn, cfn)
-                logging.info("Took %s seconds to copy  %s bytes to %s",
-                             time.time() - stime, totSize, cfn)
-
+            for row in frames.to_dict(orient="records"):
+                t = row["t"].to_pydatetime()
+                del row["t"]
+                nc.put(t, row)
+        db.commit()
 
 if __name__ == "__main__":
     from TPWUtils import Logger
+    from TPWUtils.Thread import Thread
 
     parser = ArgumentParser()
     Logger.addArgs(parser)
+    ncWriter.addArgs(parser)
     parser.add_argument("directory", type=str, nargs="+", help="Directories to look in")
-    parser.add_argument("--nc", type=str, required=True, help="Output NetCDF directory")
+    parser.add_argument("--nc", type=str, action="append", required=True, help="Output NetCDF filenames")
     parser.add_argument("--db", type=str, default="arcterx", help="Database name")
     parser.add_argument("--config", type=str, required=True, 
                         help="YAML variable definitions")
-    parser.add_argument("--copyTo", type=str, help="Directory to copy files to, atomically")
-    parser.add_argument("--bufferSize", type=int, default=1024*1024, help="Buffer size for copy to")
     args = parser.parse_args()
 
-    Logger.mkLogger(args, fmt="%(asctime)s %(levelname)s: %(message)s")
+    Logger.mkLogger(args)
 
     try:
+        with open(args.config, "r") as fp: varDefs = yaml.safe_load(fp)
+        logging.info("varDefs %s", varDefs)
+
         directories = []
         for directory in args.directory:
             directories.append(os.path.abspath(os.path.expanduser(directory)))
 
-        loadIt(directories, args)
+        nc = ncWriter(args, args.nc, varDefs)
+        nc.start()
+
+        loadIt(directories, args, nc)
+
+        nc.put(None, None)
+
+        Thread.waitForException()
+    except UserWarning:
+        logging.info("Finished")
     except:
         logging.exception("GotMe")
